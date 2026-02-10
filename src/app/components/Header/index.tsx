054a8dd47f6dbbaf33e53df580009cd1c769e01e
saarrr-alves <saraealvesemaia@gmail.com>
2026-02-10 15:21:04 -0300

    Config ESLint e Prettier

--- src/app/components/Header/index.tsx
+++ src/app/components/Header/index.tsx
@@ -1,13 +1,21 @@
-import style from './Header.module.css';
-import Image from 'next/image';
+import style from "./Header.module.css";
+import Image from "next/image";
 
 const Header = () => {
   return (
     <header className={style.header}>
-      <Image className={style.img} src="/favicon.ico" alt="Logo do Sweet Cuisine" width={50} height={50} />
-      <h1 className={style.title}>Bem-Vindos ao <span>Sweet Cuisine</span></h1>
+      <Image
+        className={style.img}
+        src="/favicon.ico"
+        alt="Logo do Sweet Cuisine"
+        width={50}
+        height={50}
+      />
+      <h1 className={style.title}>
+        Bem-Vindos ao <span>Sweet Cuisine</span>
+      </h1>
     </header>
   );
-}
+};
 
 export default Header;
